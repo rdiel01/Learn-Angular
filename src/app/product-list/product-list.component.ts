11cdac6aa4e78197873d758a0e4db391a5884f53
rdiel01 <rdiel01@gmail.com>
2020-03-22 22:27:23 -0500

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share(productName) {
     window.alert( productName + ' has been shared!');
   }
+
+  onNotify(thisProduct) {
+    window.alert('You will receive a notification when ' + thisProduct.name + ' is less than $' + thisProduct.price +'.00.')
+  }
 }
 
 
